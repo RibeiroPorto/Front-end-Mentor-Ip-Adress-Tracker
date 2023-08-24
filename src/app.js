ece9807d13d94b397aeb77c6764ae7912550e0dc
Vinicius Ribeiro <portov394@gmail.com>
2023-08-23 21:38:29 -0300

    modified api

--- src/app.js
+++ src/app.js
@@ -1,89 +1,84 @@
+import API_KEY from './config.js'
 document.addEventListener('DOMContentLoaded', () => {
-
-
-    const apiEndpoint = "http://ip-api.com/json"
+    console.log(API_KEY)
+    const apiEndpoint = "https://api.ipgeolocation.io/ipgeo?apiKey="
     const ipInput = document.querySelector("#ip_or_domain")
     const ipResult = document.querySelector('#ip-result')
     const locationResult = document.querySelector('#location-result')
     const timeZoneResult = document.querySelector('#timezone-result')
     const ispResult = document.querySelector('#isp-result')
 
-    map = setMap()
-    marker = setMarker(map, { lat: 51.505, lon: -0.09 })
+    var map = setMap()
+    var marker = setMarker(map, { latitude: 51.505, longitude: -0.09 })
 
-    console.log(map)
-    console.log(marker)
     document.querySelector('#submit-button').addEventListener('click', async () => {
-        ipData = await getData(apiEndpoint, ipInput.value)
-        if (dataValidation(ipData)) {
+        var ipData = await getData(apiEndpoint, API_KEY, ipInput.value)
+        console.log(ipData)
+        if (dataIsValid(ipData)) {
             displayResults(ipData)
             displayMap(ipData, map)
             if (marker) { marker.remove() }
             marker = setMarker(map, ipData)
-            
+        }else{
+            ipInput.classList.add('invalid_entry')
+            ipInput.value = ""
+            ipInput.placeholder = "Invalid entry, please try again."
+            setTimeout(() => {
+                ipInput.classList.remove('invalid_entry')
+                ipInput.placeholder = "Search for any IP address or domain"
+            }, 2000)
         }
-
     })
-    const displayMap = (data, map) => {
-        const lat = data.lat
-        const lon = data.lon
-
-        console.log(lat, lon)
-        map.setView([lat, lon], 13);
-
-    }
     const displayResults = (data) => {
-        const ip = data.query
-        const location = data.city + ", " + data.region
-        const timeZone = parseInt(data.offset) / 3600
+        const ip = data.ip
+        const location = data.city + ", " + data.country_name + ", " + data.zipcode
+        const timeZone = parseInt(data.time_zone.offset) 
         const isp = data.isp
-
+    
         if (timeZone.toString().length < 2) {
-            // console.log(timeZone.toString())
-            // console.log(timeZone.toString().length)
-
             utcTime = "UTC 0" + timeZone.toString() + ":00"
-
         } else if (timeZone.toString().length < 3) {
             if (timeZone.toString()[0] === "-") {
-
-                utcTime = "UTC-0" + timeZone.toString()[timeZone.toString().length - 1] + ":00"
+    
+                var utcTime = "UTC-0" + timeZone.toString()[timeZone.toString().length - 1] + ":00"
             } else {
-                utcTime = "UTC" + timeZone.toString() + ":00"
+                var utcTime = "UTC" + timeZone.toString() + ":00"
             }
         } else {
-            utcTime = "UTC" + timeZone.toString() + ":00"
+            var utcTime = "UTC" + timeZone.toString() + ":00"
         }
         ipResult.innerHTML = "<p>" + ip + "</p>"
         locationResult.innerHTML = "<p>" + location + "</p>"
         timeZoneResult.innerHTML = "<p>" + utcTime + "</p>"
         ispResult.innerHTML = "<p>" + isp + "</p>"
     }
-    const dataValidation = (data) => {
-
-        switch (data.status) {
-            case 'fail':
-                console.log(data)
-                ipInput.classList.add('invalid_entry')
-                ipInput.value = ""
-                ipInput.placeholder = "Invalid entry, please try again."
-                setTimeout(() => {
-                    ipInput.classList.remove('invalid_entry')
-                    ipInput.placeholder = "Search for any IP address or domain"
-                }, 2000)
-                break;
-            case 'success':
-                
-                return true
-                break
-        }
-
-    }
 
 })
-async function getData(endPoint, ip) {
-    console.log(endPoint + "/" + ip + "?fields=63172607")
-    const response = await fetch(endPoint + "/" + ip + "?fields=63172607")
+const displayMap = (data, map) => {
+    const lat = data.latitude
+    const lon = data.longitude
+    console.log(lat, lon)
+    map.setView([lat, lon], 13);
+
+}
+
+const dataIsValid = (data) => {
+
+    switch (data.message) {
+        case undefined:
+            
+            return true
+            break
+        default:
+            return false
+            
+            break;
+    }
+
+}
+async function getData(endPoint,API_KEY, ip) {
+    console.log(endPoint + API_KEY + "&ip=" + ip + "&"+"Accept-Charset: UTF-8")
+    const response = await fetch(endPoint + API_KEY + "&ip=" + ip + "&"+"Accept-Charset: UTF-8")
     const jsonData = await response.json()
     return jsonData
 }
@@ -98,8 +93,8 @@ function setMap() {
     return map
 }
 function setMarker(map, data) {
-    const lat = data.lat
-    const lon = data.lon
+    const lat = data.latitude
+    const lon = data.longitude
     var marker = L.marker([lat, lon]).addTo(map);
     marker.bindPopup(`<b>Lat: </b>${lat}<br><b>Lat: </b>${lon}`).openPopup();
     marker._icon.src = './images/icon-location.svg'
---
+++ src/config.js
@@ -0,0 +1,2 @@
+const API_KEY = '6ba4e17674b24d0b897e195648bf6ac8'
+export default API_KEY
